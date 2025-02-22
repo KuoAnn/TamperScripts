2ce00fb15a037b746381429d26f5063221f18486
KuoAnn <b54744040@gmail.com>
2025-02-22 17:23:52 +0800

    feat(baozi): 更新版本至 1.0.3 並新增鍵盤控制功能
    - 更新腳本版本至 1.0.3。
    - 新增對 https://www.baozimh.com/comic/* 的支援。
    - 改善警示訊息的顯示方式，使用 GM_addStyle 來設置樣式。
    - 增加鍵盤控制功能以便於翻頁，並優化章節目錄的顯示。
    - 實作章節排序功能，確保章節按正確順序顯示。

--- src/baozi.user.js
+++ src/baozi.user.js
@@ -1,35 +1,65 @@
 // ==UserScript==
 // @name         Baozi
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  包子漫畫內透過鍵盤控制翻頁，W: 上滾，S: 下滾，A: 上一頁，D: 下一頁
 // @author       KuoAnn
 // @match        https://www.twmanga.com/comic/chapter/*
+// @match        https://www.baozimh.com/comic/*
 // @icon         https://www.google.com/s2/favicons?sz=16&domain=twmanga.com
 // @downloadURL  https://github.com/KuoAnn/TamperScripts/raw/main/src/baozi.user.js
 // @updateURL    https://github.com/KuoAnn/TamperScripts/raw/main/src/baozi.user.js
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
-// 系統參數
+GM_addStyle(".alertContainer{position:fixed;top:6px;left:6px;z-index:9999;pointer-events:none;}");
+GM_addStyle(".alertMessage{background:rgba(94,39,0,0.7);color:white;padding:4px;margin:4px;border-radius:5px;pointer-events:auto;font-size:14px;}");
+
 let _isLoaded = false;
-let _isSubmit = false;
+let loader;
+let messages = [];
+
+// override alert
+const alertContainer = document.createElement("div");
+alertContainer.className = "alertContainer";
+document.body.appendChild(alertContainer);
+
+const alert = (function () {
+    return function (str) {
+        var message = document.createElement("div");
+        message.className = "alertMessage";
+        message.innerText = str;
+        alertContainer.appendChild(message);
+        messages.push(message);
+
+        if (messages.length > 20) {
+            var oldMessage = messages.shift();
+            if (alertContainer.contains(oldMessage)) {
+                alertContainer.removeChild(oldMessage);
+            }
+        }
+
+        setTimeout(function () {
+            if (alertContainer.contains(message)) {
+                alertContainer.removeChild(message);
+            }
+        }, 3333);
+    };
+})();
 
 (function () {
     "use strict";
 
-    const observer = new MutationObserver((mo) => {
-        mo.forEach((mutation) => {
-            if (mutation.type === "childList") {
-                console.log("Mutated");
-            }
-        });
-    });
-    observer.observe(document.body, { childList: true, subtree: true });
+    if (window.location.hostname === "www.baozimh.com") {
+        console.log("trigger");
+        loader = setInterval(() => {
+            handleLoader();
+        }, 500);
+    }
 
+    // hotkey
     const handleKeydown = (e) => {
         const links = document.querySelectorAll(".next_chapter a");
-        console.log(e.key)
         switch (e.key) {
             case "w":
                 window.scrollBy({
@@ -59,6 +89,57 @@ let _isSubmit = false;
                 break;
         }
     };
-
     document.addEventListener("keydown", handleKeydown);
+
+    function handleLoader() {
+        const sectionTitles = document.querySelectorAll(".section-title");
+        if (sectionTitles && !_isLoaded) {
+            _isLoaded = true;
+            clearInterval(loader);
+            console.log("Loaded");
+
+            sectionTitles.forEach((sectionTitle) => {
+                if (sectionTitle.textContent.indexOf("最新章節") > -1) {
+                    // console.log("優化最新章節");
+                    // sortCapters(sectionTitle.nextElementSibling);
+                } else if (sectionTitle.textContent.indexOf("章節目錄") > -1) {
+                    console.log("優化章節目錄");
+                    console.log("將其他章節加入章節目錄");
+                    const chapterItems = document.querySelector("#chapter-items");
+                    const chapters_other_list = document.querySelector("#chapters_other_list");
+                    if (chapters_other_list) {
+                        // 將其他章節加入章節目錄
+                        const otherList = chapters_other_list.querySelectorAll(":scope > div");
+                        otherList.forEach((chapter) => {
+                            chapterItems.appendChild(chapter);
+                        });
+                    }
+                    sortCapters(chapterItems);
+                }
+            });
+        }
+    }
+
+    function sortCapters(eleChapters) {
+        let chapters = eleChapters.querySelectorAll(":scope > div");
+        chapters = Array.from(chapters).sort((a, b) => {
+            try {
+                const numA = parseFloat(a.textContent.match(/第(\d+)話/)[1]);
+                const numB = parseFloat(b.textContent.match(/第(\d+)話/)[1]);
+                return numB - numA;
+            } catch (error) {
+                const errMsg = `Sort Error: ${error} ${a.textContent}|${b.textContent}`;
+                alert(errMsg);
+                console.error(errMsg);
+                // 預設反序
+                return a > b ? 1 : -1;
+            }
+        });
+
+        // 清空原有的章節並重新加入反序後的章節
+        eleChapters.innerHTML = "";
+        Array.from(chapters).forEach((chapter) => {
+            eleChapters.appendChild(chapter);
+        });
+    }
 })();
